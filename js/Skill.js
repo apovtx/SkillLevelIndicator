30311e19a4ac67b21d9d8728538c1cfa05d63e76
Apostolos Voulkidis <apovtx@gmail.com>
2012-08-12 16:01:55 +0300

    First Commit

---
+++ js/Skill.js
@@ -0,0 +1,41 @@
+﻿/* 
+	Author: Apostolos Voulkidis
+	Website: http://apovtx.eu
+*/
+
+
+function SkillIndicator(context, options) {
+		var options = options || {};
+
+		if(typeof context == 'undefined') {
+			return undefined;
+		}
+		
+		var color = options.color || "#8ED6FF",
+			borderColor = options.borderColor|| "#000", 
+			X = options.X || 1,
+			Y = options.Y || 1,
+			level = options.level || 50,
+			name = options.name || "canvas",
+			radius = options.radius || 70,
+			lineWidth = options.lineWidth || 15,
+			fontSize = options.fontSize || '35pt',
+			font = options.font || "italic " + fontSize + " Calibri",
+			fontColor = options.fontColor || "white";
+		
+		context.beginPath();
+		context.arc(X, Y, radius, 0, 2 * Math.PI, false);
+		context.fillStyle = color;
+		context.fill();
+    
+		context.beginPath();
+		context.arc(X, Y, radius-lineWidth/2, 0, level/50 * Math.PI, false);
+		context.lineWidth = lineWidth;
+		context.strokeStyle = borderColor;
+		context.stroke();
+		
+		context.font = font;
+		context.textAlign = 'center';
+		context.fillStyle = fontColor;
+		context.fillText(name, X, Y+(radius*0.20));
+}
